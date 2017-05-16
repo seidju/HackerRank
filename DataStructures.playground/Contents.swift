332f3c6fb94cd9d7a84201895b49c0a428d2f79d
Pavel Shatalov <shatalovp@gmail.com>
2017-05-16 12:47:30 +0700

    add descriptions

--- DataStructures.playground/Contents.swift
+++ DataStructures.playground/Contents.swift
@@ -17,6 +17,6 @@ for i in 0...arr.count-3 {
         sum.append(arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2])
     }
 }
-print(sum.max()!)
+print("MAX SUMM OF HOURGLASS IS \(sum.max()!)")
 
 
