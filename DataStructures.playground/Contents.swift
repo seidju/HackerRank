9379da08c91dbb179e5b536b23a5f28da16c1357
Pavel Shatalov <shatalovp@gmail.com>
2017-05-17 10:13:49 +0700

    Add algorithmic crush

--- DataStructures.playground/Contents.swift
+++ DataStructures.playground/Contents.swift
@@ -52,3 +52,79 @@ let rotations:Int = 5
 let rotated = Array(inputArray[rotations ..< inputArray.count] + inputArray[0 ..< rotations])
 print(rotated.map{"\($0) "}.reduce("",+)) // to string
 
+
+//MARK: - Sparse Arrays
+//https://www.hackerrank.com/challenges/sparse-arrays
+
+let numStr: Int = 4
+let strings = ["aba", "baba", "aba", "xzxb"]
+let numQueries: Int = 3
+let strQueries = ["aba", "xzxb", "ab"]
+
+for query in strQueries {
+    var count = 0
+    for str in strings {
+        if str == query {count += 1}
+    }
+    print(count)
+}
+
+
+
+//MARK: - Algorithmic Crush
+//https://www.hackerrank.com/challenges/crush
+
+let listSize = 5
+let M = 3
+let operations = [[1,2, 100], [2,5,100], [3,4,100]]
+var list = [Int]()
+for _ in 0...listSize{
+    list.append(0)
+}
+
+operations.forEach { op in
+    let a:Int = op[0] - 1
+    let b:Int = op[1] - 1
+    let val:Int = op[2]
+    //print(a,b,val)
+    list.enumerated().forEach {idx, element in
+        if idx >= a && idx <= b {
+            list[idx] = element + val
+        }
+    }
+}
+print(list.sorted {$0>$1}.first!) // this solution is slow , because of O(N2)
+
+var newList = [Int]()
+for _ in 0...listSize{
+    newList.append(0)
+}
+//better solution
+
+//In this case we use only one for-loop
+//The main idea is that when we add sum to first interval index
+//we need to substract this value from outter interval index
+//after sum all values max value will be found
+for i in 0..<M {
+    let op = operations[i]
+    let p:Int = op[0]-1
+    let q:Int = op[1]-1
+    let sum:Int = op[2]
+    newList[p] += sum
+    if q+1 <= listSize {
+        newList[q+1] -= sum
+    }
+}
+var max = 0
+var x = 0
+for i in 0..<listSize {
+    x = x + newList[i]
+    max = max < x ? x: max
+}
+print(max)
+
+
+
+
+
+
